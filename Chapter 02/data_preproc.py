7df1ce2e2b834e4ebbded67ea2bea626858a630b
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-04-17 23:26:50 +0000

    Update data_preproc.py

--- Chapter 02/data_preproc.py
+++ Chapter 02/data_preproc.py
@@ -9,6 +9,24 @@ input_data = np.array([[5.1, -2.9, 3.3],
                       ])
 
 # Binary data 
+# Explanation if each scalar sup of treshold put 1 else put 0
+
 data_binarize = preprocessing.Binarizer(threshold=2.1).transform(input_data)
 print('\n Binarize data : \n',data_binarize)
 
+# Print mean and standart deviation :
+print('\n Before:')
+print('Mean = ', input_data(axis = 0))
+print('Std deviation =', input_data.std(axis = 0))
+
+# Min max scaling
+data_scaler_minmax = preprocessing.MinMaxScaler(feature_range=(0,1))
+data_scaled_minmax = data_scaler_minmax.fit_transform(input_data)
+print('\n Min max scaled data : ', data_scaled_minmax)
+
+# Normalize data
+data_normalized_l1 = preprocessing.normalize(input_data, norm = 'l1')
+data_normalized_l2 = preprocessing.normalize(input_data, norm='l2')
+
+print('\n L1 normalized data : \n', data_normalized_l1)
+print('\nL2 normalized data: \n', data_normalized_l2)
